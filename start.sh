30fa3b1826acecb566ecfdb737762d723da98297
l3oku <121317836+l3oku@users.noreply.github.com>
2023-05-06 05:07:27 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 设置argo token
-TOK=${TOK:-'cloudflared.exe service install eyJhIjoiNTRhM2QyMDEwZTk0YmU5MDA3NWQxZmI0NGQ4ZTg2YWEiLCJ0IjoiZWU0OWI2ZWUtOWNjMC00NDdkLTgzMGItZjY0ZWRjNjAxZTQ0IiwicyI6IlpURTVOVGhqTmpNdE1USXdOaTAwTlRWaExUa3hZekV0WVdJNU5ETmhNRFk1Wm1NNCJ9'}
+TOK=${TOK:-'cloudflared.exe service install eyJhIjoiM2M5ZjA1YTFlNDNkZTRlMTBiODhjZWYzODA1NmE4ZWUiLCJ0IjoiNWU3NTdlYzItMzdmMi00NDQxLWJjN2UtMzhiM2U0NGJkNTQ0IiwicyI6Ik5EUTJaalUzTmpjdFpqRm1OUzAwWmpWaExXRTRNMk10WTJGaE1qTTBOMkkwWldWaSJ9'}
 
 # 设置argo下载地址
 URL_CF=${URL_CF:-'github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64'}
